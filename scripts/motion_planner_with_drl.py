64ac44d2276725b6b29f15ff599217f19b8f491c
Jumpei Arima <arijun0307@gmail.com>
2019-06-21 18:15:55 +0900

    update

--- scripts/motion_planner_with_drl.py
+++ scripts/motion_planner_with_drl.py
@@ -9,8 +9,10 @@ from nav_msgs.msg import Odometry
 import tf
 import math
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-NN_MODEL = os.path.join(os.path.dirname(os.path.abspath("__file__")),'models/ppo_model_ep_100000.ckpt')
+# NN_MODEL = os.path.join(os.path.dirname(os.path.abspath("__file__")),'models/ppo_model_ep_100000.ckpt')
+NN_MODEL = ('/home/amsl/ros_catkin_ws/src/motion_planner_with_drl/scripts/models/ppo_model_ep_100000.ckpt')
 print(NN_MODEL)
 
 lidar = np.zeros(36)
@@ -34,7 +36,8 @@ def angle_diff(a,b):
         return d2
 
 def get_yaw(q):
-    e = tf.transformations.euler_from_quaternion(q)
+    quaternion = np.array((q.x, q.y, q.z, q.w))
+    e = tf.transformations.euler_from_quaternion(quaternion)
     return e[2]
 
 def callback_target(data):
@@ -45,7 +48,7 @@ def callback_target(data):
 
 def callback_laser(data):
     global lidar
-    for i in range(len(lidar)):#-1,-1,-1):
+    for i in range(len(lidar)):
         l = []
         for j in range(i*20,(i+1)*20-1):
             l.append(data.ranges[j])
@@ -62,35 +65,16 @@ if __name__ == '__main__':
 
     rospy.init_node('motion_planner_with_drl')
     laser_sub = rospy.Subscriber('/local_map/scan',LaserScan, callback_laser)
-    target_pub = rospy.Subscriber('/direction/relative',PoseStamped,callback_target)
-    vel_pub = rospy.Publisher('/tinypower/command_velocity',Twist,queue_size = 100)
-
-    # listener = tf.TransformListener()
+    target_sub = rospy.Subscriber('/direction/relative',PoseStamped,callback_target)
+    vel_pub = rospy.Publisher('/cmd_vel',Twist,queue_size = 100)
 
     rate = rospy.Rate(10)
     vel = Twist()
-    target_orientation = PoseStamped()
 
     while not rospy.is_shutdown():
-        # try:
-        #     (trans,rot) = listener.lookupTransform("localmap","base_link",rospy.Time(0.0))
-        # except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        #     continue
-        # yaw = get_yaw(rot)
+        print("=======motion planner with DRL========")
         if target_sub_:
             theta = target_yaw
-            print(theta)
-            # target_orientation.header.frame_id = "localmap"
-            target_orientation.header.frame_id = "base_link"
-            target_orientation.header.stamp = rospy.Time.now() 
-            target_orientation.pose.position.x = 0 
-            target_orientation.pose.position.y = 0 
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, theta)
-            target_orientation.pose.orientation.x = quaternion[0]
-            target_orientation.pose.orientation.y = quaternion[1]
-            target_orientation.pose.orientation.z = quaternion[2]
-            target_orientation.pose.orientation.w = quaternion[3]
-            target_pub.publish(target_orientation)
 
             s = np.zeros(39)
             min_lidar = 10.0
@@ -102,7 +86,6 @@ if __name__ == '__main__':
             s[36] = 5.0 
             s[37] = np.sin(theta)
             s[38] = np.cos(theta)
-            #print(s[36])
             s = np.array([s])
             action = brain.predict_a(s).reshape(-1)
             vel.linear.x = action[0]
@@ -114,7 +97,7 @@ if __name__ == '__main__':
                 if min_index > 18:
                     a = -1
                 vel.angular.z = a*0.3
-                print("aaaaaa")
-            print(action[0],action[1])
+                print("=====safety stop=====")
+            print("action: v=%f[m/s], w=%f[rad/s]" % (action[0],action[1]))
             vel_pub.publish(vel)
         rate.sleep()
