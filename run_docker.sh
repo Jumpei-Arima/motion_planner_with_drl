0734d170a648b88548f5ece8645066afefd9716b
Jumpei Arima <arijun0307@gmail.com>
2019-09-13 19:07:43 +0900

    update

--- run_docker.sh
+++ run_docker.sh
@@ -8,9 +8,8 @@ docker run -it --rm \
   --env=QT_X11_NO_MITSHM=1 \
   --env=DISPLAY=$DISPLAY \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="${PWD}:/root/catkin_ws/src/motion_planner_with_drl" \
+  --volume="${PWD}/models:/root/models" \
   --net='host' \
   --name="ros_mpdrl" \
   arijun/ros_mpdrl \
-  bash
-
+  bash -c "roslaunch motion_planner_with_drl local_planner.launch"
