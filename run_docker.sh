ccad335d05d1d1ac0d9d6fca84a79d93b71ae807
Jumpei Arima <arijun0307@gmail.com>
2019-10-19 18:46:21 +0900

    update

--- run_docker.sh
+++ run_docker.sh
@@ -2,9 +2,19 @@
 
 echo "=== run_docker ==="
 
+if [ $# -eq 0 ];
+then
+    cmd="bash"
+else
+    cmd="catkin_make && "$@
+    cmd="bash -c "${cmd}
+fi
+
+echo ${cmd}
+
 docker run -it --rm \
   --volume="${PWD}:/root/catkin_ws/src/mpdrl_ros" \
   --net='host' \
-  --name="mpdrl" \
-  arijun/mpdrl \
-  bash -c "catkin_make && roslaunch mpdrl_ros local_planner.launch"
+  --name="mpdrl_ros" \
+  arijun/mpdrl_ros \
+  ${cmd}
